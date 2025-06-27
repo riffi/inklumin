1229c46b15a2a90c72fe0e6a3bba2552ea3112cb
Belenko Vladimir <vbelenko@ntdev.ru>
2025-06-27 11:03:40 +0300

    [main] при переходе на страницу сцен главное меню сворачивается

--- inklumin-front/src/pages/scenes/ScenesPage.tsx
+++ inklumin-front/src/pages/scenes/ScenesPage.tsx
@@ -1,7 +1,18 @@
+import { useEffect } from "react";
 import { Box } from "@mantine/core";
 import { SceneLayout } from "@/components/scenes/SceneLayout/SceneLayout";
+import { useUiSettingsStore } from "@/stores/uiSettingsStore/uiSettingsStore";
 
 export const ScenesPage = () => {
+  const { isNavbarOpened, toggleNavbarOpened } = useUiSettingsStore();
+
+  useEffect(() => {
+    // При переходе на страницу сцен сворачиваем меню
+    if (isNavbarOpened) {
+      toggleNavbarOpened();
+    }
+  }, []);
+
   return (
     <Box>
       <SceneLayout />
