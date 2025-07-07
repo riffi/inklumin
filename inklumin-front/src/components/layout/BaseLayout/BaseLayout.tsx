51914a0287662fc8068b4888445eba5fc56e6e88
Belenko Vladimir <vbelenko@ntdev.ru>
2025-07-07 16:17:08 +0300

    [main] вертска действий в мобильном хеадере приведена к стандартному виду RowActionButtons

--- inklumin-front/src/components/layout/BaseLayout/BaseLayout.tsx
+++ inklumin-front/src/components/layout/BaseLayout/BaseLayout.tsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
-import { IconDots } from "@tabler/icons-react";
 import { Outlet } from "react-router-dom";
-import { ActionIcon, AppShell, Box, Burger, Drawer, Group, Stack, Text } from "@mantine/core";
+import { AppShell, Box, Burger, Group, Text } from "@mantine/core";
 import { NavbarNested } from "@/components/layout/NavbarNested/NavbarNested";
 import { IconViewer } from "@/components/shared/IconViewer/IconViewer";
+import {
+  ActionItem,
+  RowActionButtons,
+} from "@/components/shared/RowActionButtons/RowActionButtons";
 import { useBookDbConnection } from "@/hooks/useBookDbConnection";
 import { useMedia } from "@/providers/MediaQueryProvider/MediaQueryProvider";
 import { useMobileHeader } from "@/providers/PageTitleProvider/MobileHeaderProvider";
@@ -12,7 +14,6 @@ import { useUiSettingsStore } from "@/stores/uiSettingsStore/uiSettingsStore";
 export const BaseLayout = () => {
   const { isNavbarOpened, toggleNavbarOpened } = useUiSettingsStore();
   const { header } = useMobileHeader();
-  const [drawerOpened, setDrawerOpened] = useState(false);
   // Подключаемся к базе данных выбранного произведения
   useBookDbConnection();
   const { isMobile } = useMedia();
@@ -50,50 +51,38 @@ export const BaseLayout = () => {
               }}
             >
               {isMobile && header && (
-                <Group gap="sm" justify="center" align="center" style={{display: "flex"}}>
-                  <Group style={{flexGrow: 1, justifyContent: "center"}}>
-                    {header.icon && <IconViewer icon={header.icon} size={24} color={"var(--mantine-color-gray-6)"} />}
-                    <Text fw={500} color={"var(--mantine-color-gray-6)"}>{header.title}</Text>
+                <Group gap="sm" justify="center" align="center" style={{ display: "flex" }}>
+                  <Group style={{ flexGrow: 1, justifyContent: "center" }}>
+                    {header.icon && (
+                      <IconViewer
+                        icon={header.icon}
+                        size={24}
+                        color={"var(--mantine-color-gray-6)"}
+                      />
+                    )}
+                    <Text fw={500} color={"var(--mantine-color-gray-6)"}>
+                      {header.title}
+                    </Text>
                   </Group>
                   {header.actions && header.actions.length > 0 && (
-                    <ActionIcon variant="subtle" onClick={() => setDrawerOpened(true)}>
-                      <IconDots size={20} />
-                    </ActionIcon>
+                    <RowActionButtons
+                      actions={
+                        header.actions.map((a, idx) => ({
+                          key: idx.toString(),
+                          title: a.title,
+                          icon: a.icon,
+                        })) as ActionItem[]
+                      }
+                      onAction={(key) => {
+                        const action = header.actions[Number(key)];
+                        action.handler();
+                      }}
+                    />
                   )}
                 </Group>
               )}
             </Box>
-            {header?.actions && (
-              <Drawer
-                opened={drawerOpened}
-                onClose={() => setDrawerOpened(false)}
-                position="bottom"
-                size="25%"
-              >
-                <Stack gap="0">
-                  {header.actions.map((action, index) => (
-                    <Group
-                      key={index}
-                      gap="sm"
-                      onClick={() => {
-                        action.handler();
-                        setDrawerOpened(false);
-                      }}
-                      style={{
-                        cursor: "pointer",
-                        paddingTop: 8,
-                        paddingBottom: 8,
-                        borderBottom:
-                          index < header.actions.length - 1 ? "1px solid rgba(0,0,0,0.1)" : "none",
-                      }}
-                    >
-                      <ActionIcon variant="subtle">{action.icon}</ActionIcon>
-                      <Text size="lg">{action.title}</Text>
-                    </Group>
-                  ))}
-                </Stack>
-              </Drawer>
-            )}
+            {/** RowActionButtons уже содержит Drawer */}
           </Group>
         </AppShell.Header>
 
