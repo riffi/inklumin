87b885b6610ca15f4a082f23173ca73d3d1adcaa
Belenko Vladimir <vbelenko@ntdev.ru>
2025-07-09 10:07:49 +0300

    [main] изменен baseurl в docosaurus

--- inklumin-docs/docusaurus.config.ts
+++ inklumin-docs/docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://inklumin.ru',
+  url: 'https://inklumin.ru/docs',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs',
+  baseUrl: '/',
 
 
 
